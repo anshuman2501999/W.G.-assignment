84428091fd6fb3f1e5920895ffff39cdcff5746c
anshuman2501999 <60056918+anshuman2501999@users.noreply.github.com>
2021-09-07 10:41:22 +0530

    Add files via upload

---
+++ W.G assisgnment/data.py
@@ -0,0 +1,25 @@
+def Products():
+    produts=[
+        {
+            'id':'1',
+            'name':'New Fashion Portable clip Mini USB MP3 Music Media Player', 
+            'price':'4000'
+        },
+        {
+            'id':'2',
+            'name':'Sony walkman mp3 Media Player', 
+            'price':'5999'
+        },
+
+        {
+            'id':'3',
+            'name':'Apple i-pod Media Player',
+            'price':'15999'
+        }
+
+        
+
+
+    ]
+
+    return products
